51190aca0149d14b01694f0022b7c019d03d0649
Gábor József Wacha <wachag@gmail.com>
2016-10-13 16:36:44 +0200

    Test version

--- minefield.py
+++ minefield.py
@@ -21,7 +21,6 @@ def generateguess(N):
 
 def generatescore(gamestate, guess):
     if guess in gamestate[1]:
-        print("BOOM")
         return (True, -(gamestate[0].size * gamestate[0].size))
     numcovered = numpy.count_nonzero(gamestate[0] + 1)
     if gamestate[0][guess[0]][guess[1]] != -1:
@@ -43,54 +42,65 @@ def generatestate(gamestate, guess):
     return newstate
 
 
-SIZE = 6
-NMINES = 5
+SIZE = 16
+NMINES = 40
 
 # jutalom: új és nem akna
 # büntetés: régi (kicsi), akna(nagy)
 
 
 model = Sequential([
-    Dense(32, input_dim=SIZE * SIZE),
+    Dense(SIZE*int(numpy.sqrt(SIZE)), input_dim=SIZE * SIZE),
     Activation('relu'),
-    Dense(20),
+    Dense(SIZE),
     Activation('softmax'),
     Dense(SIZE * SIZE),  # outputs: Coordinates
     Activation('softmax'),
 ])
+
 model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
-
-epsilon = 1
-gamma = 0.99
-epochs = 10000
-originalstate = generateminefield(SIZE, NMINES)
-for i in range(1, epochs):
-    gamestate=copy.copy(originalstate)
-    status = 1
-    while status == 1:
-        qval = model.predict(gamestate[0].reshape(1, SIZE * SIZE))
-        if (random.random() < epsilon):
-            guess = generateguess(SIZE)
-        else:
-            a = int(numpy.argmax(qval[0]))
-            guess = [int(a / SIZE), a % SIZE]
-        (died, reward) = generatescore(gamestate, guess)
-        if not died:
-            newstate = generatestate(gamestate, guess)
-        else:
-            newstate = gamestate
-        newq = model.predict(gamestate[0].reshape(1, SIZE * SIZE))
-        update = reward + gamma * numpy.max(newq)
-        y = numpy.zeros((1, SIZE * SIZE))
-        y[:] = qval[:]
-
-        y[0][guess[0] * SIZE + guess[1]] = update
-        model.fit(gamestate[0].reshape(1, SIZE * SIZE), y, batch_size=1, nb_epoch=1, verbose=1)
-        gamestate = newstate
-        numcovered = numpy.count_nonzero(gamestate[0] + 1)
-        if (numcovered==SIZE*SIZE-NMINES) or died:
+startepsilon = 1
+for game in range(1,1000):
+    print("Playing game ",game)
+    epsilon=startepsilon
+    if startepsilon > 0.1:
+        startepsilon -= (1 / game)
+    gamma = 0.9
+    epochs = 10000
+    originalstate = generateminefield(SIZE, NMINES)
+    iter=0
+    for i in range(1, epochs):
+        gamestate=copy.copy(originalstate)
+        status = 1
+        print("Teaching ",i)
+        iter=0
+        while status == 1:
+            qval = model.predict(gamestate[0].reshape(1, SIZE * SIZE))
+            if (random.random() < epsilon):
+                guess = generateguess(SIZE)
+            else:
+                a = int(numpy.argmax(qval[0]))
+                guess = [int(a / SIZE), a % SIZE]
+            (died, reward) = generatescore(gamestate, guess)
             if not died:
-                print("Yeehaw!")
-            status=0
-    if epsilon > 0.1:
-        epsilon -= (1/epochs)
+                newstate = generatestate(gamestate, guess)
+            else:
+                newstate = gamestate
+            newq = model.predict(gamestate[0].reshape(1, SIZE * SIZE))
+            update = reward + gamma * numpy.max(newq)
+            y = numpy.zeros((1, SIZE * SIZE))
+            y[:] = qval[:]
+
+            y[0][guess[0] * SIZE + guess[1]] = update
+            model.fit(gamestate[0].reshape(1, SIZE * SIZE), y, batch_size=1, nb_epoch=i, verbose=0)
+            gamestate = newstate
+            numcovered = numpy.count_nonzero(gamestate[0] + 1)
+            if (numcovered==SIZE*SIZE-NMINES) or died:
+                if not died:
+                    print("Game", game," won after ", iter, " iterations")
+                else:
+                    print("Game", game," lost after ", iter, " iterations")
+                status=0
+            iter=iter+1
+        if epsilon > 0.1:
+            epsilon -= (1/epochs)
