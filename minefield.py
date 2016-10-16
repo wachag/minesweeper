80a3f9c6edaf3c68b4080084a8771bb668b5bdb4
Gábor József Wacha <wachag@gmail.com>
2016-10-16 06:56:58 +0200

    Fixes and plots

--- minefield.py
+++ minefield.py
@@ -1,23 +1,21 @@
-import numpy
+﻿import numpy
 import random
 import copy
+import time
 from keras.models import Sequential
 from keras.layers import Dense, Activation
-
+import matplotlib.pyplot as plt
+SIZE = 10
+NMINES = 5
 
 def generateminefield(N, Nmines):
     field = -numpy.ones((N, N), numpy.int8)
-
-    # x=numpy.array([i] for i in range(1,N))
-    # y=numpy.array([i] for i in range(1,N))
     mines = [[x0, y0] for x0 in range(1, N) for y0 in range(1, N)]
     random.shuffle(mines)
     return (field, mines[0:Nmines])
 
-
 def generateguess(N):
     return [random.randrange(1, N), random.randrange(1, N)]
-
 
 def generatescore(gamestate, guess):
     if guess in gamestate[1]:
@@ -40,18 +38,17 @@ def generatestate(gamestate, guess):
 
     newstate[0][guess[0]][guess[1]] = neighbours
     return newstate
+    
+plt.ion()    
 
 
-SIZE = 16
-NMINES = 40
-
-# jutalom: új és nem akna
-# büntetés: régi (kicsi), akna(nagy)
 
 
 model = Sequential([
     Dense(SIZE*int(numpy.sqrt(SIZE)), input_dim=SIZE * SIZE),
     Activation('relu'),
+    Dense(SIZE),
+    Activation('softmax'),
     Dense(SIZE),
     Activation('softmax'),
     Dense(SIZE * SIZE),  # outputs: Coordinates
@@ -60,8 +57,21 @@ model = Sequential([
 
 model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
 startepsilon = 1
-for game in range(1,1000):
+for game in range(1,100):
     print("Playing game ",game)
+    fig1 = plt.figure()
+    ax1 = fig1.add_subplot(111)
+    x=[a for a in range(1,2)]
+    y=[a*0 for a in range(1,2)]
+    line1, = ax1.plot(x,y,'r-')
+
+    fig2 = plt.figure()
+    ax2 = fig2.add_subplot(111)
+    x=[a for a in range(1,2)]
+    y=[a*0 for a in range(1,2)]
+    line2, = ax2.plot(x,y,'r-')
+
+
     epsilon=startepsilon
     if startepsilon > 0.1:
         startepsilon -= (1 / game)
@@ -80,7 +90,7 @@ for game in range(1,1000):
                 guess = generateguess(SIZE)
             else:
                 a = int(numpy.argmax(qval[0]))
-                guess = [int(a / SIZE), a % SIZE]
+                guess = [ a % SIZE, int(a / SIZE)]
             (died, reward) = generatescore(gamestate, guess)
             if not died:
                 newstate = generatestate(gamestate, guess)
@@ -91,7 +101,7 @@ for game in range(1,1000):
             y = numpy.zeros((1, SIZE * SIZE))
             y[:] = qval[:]
 
-            y[0][guess[0] * SIZE + guess[1]] = update
+            y[0][guess[1] * SIZE + guess[0]] = update
             model.fit(gamestate[0].reshape(1, SIZE * SIZE), y, batch_size=1, nb_epoch=i, verbose=0)
             gamestate = newstate
             numcovered = numpy.count_nonzero(gamestate[0] + 1)
@@ -99,7 +109,18 @@ for game in range(1,1000):
                 if not died:
                     print("Game", game," won after ", iter, " iterations")
                 else:
-                    print("Game", game," lost after ", iter, " iterations")
+                    print("Game", game," lost after ", iter, " iterations, covered ",numcovered," from ",SIZE*SIZE," cells.")
+                line1.set_xdata(numpy.append(line1.get_xdata(), i))
+                line1.set_ydata(numpy.append(line1.get_ydata(), numcovered*1.0/(SIZE*SIZE-NMINES)))
+                line2.set_xdata(numpy.append(line2.get_xdata(), i))
+                line2.set_ydata(numpy.append(line2.get_ydata(), iter))
+                ax1.relim()
+                ax1.autoscale_view()
+                fig1.canvas.draw()
+                ax2.relim()
+                ax2.autoscale_view()
+                fig2.canvas.draw()
+                plt.pause(1e-6) #unnecessary, but useful
                 status=0
             iter=iter+1
         if epsilon > 0.1:
